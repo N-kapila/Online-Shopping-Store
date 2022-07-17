d08e06340bde9cdf9f0cbc2607f8a60fe92b65b0
N-kapila <nirmalkapilarathne1998@gmail.com>
2022-07-17 21:39:52 +0530

    intergrate nav bar

---
+++ src/Components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div className="hero">
+            <div class="card bg-dark text-white border-0">
+                <img src="/assets/background1.jpg" class="card-img" alt="background" />
+                <div class="card-img-overlay d-flex flex-column justify-content-center">
+                    <div className="container">
+                        <h5 class="card-title display-3 fw-bolder mb-0">Happy Shopping</h5>
+                        <p class="card-text lead fs-2"> Enjoy</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
