b704a5ca42cf05b50894ad7a9c02fc435bc45617
N-kapila <nirmalkapilarathne1998@gmail.com>
2022-07-20 00:11:31 +0530

    implemented catagory dividing

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 
 const Products = () => {
     const [data, setData] = useState([]);
-    const [filter, serFilter] = useState(data);
+    const [filter, setFilter] = useState(data);
     const [loading, setLoading] = useState(false);
     let componentMounted = true;
 
@@ -12,7 +12,7 @@ const Products = () => {
             const response = await fetch("https://fakestoreapi.com/products");
             if (componentMounted) {
                 setData(await response.clone().json());
-                serFilter(await response.json());
+                setFilter(await response.json());
                 setLoading(false);
                 console.log(filter);
             }
@@ -27,31 +27,38 @@ const Products = () => {
     const Loading = () => {
         return (
             <>
-                Loading.....
+                Loading...
             </>
         )
     }
+
+    const filterProduct = (cat) => {
+        const updatedList = data.filter((x) => x.catogary === cat);
+        setFilter(updatedList);
+    }
+
     const ShowProducts = () => {
         return (
             <>
                 <div className="buttons d-flex justify-content-center mb-5 pb-5">
-                    <button className="btn btn-outline-dark me-2">All</button>
-                    <button className="btn btn-outline-dark me-2">Catogary 1</button>
-                    <button className="btn btn-outline-dark me-2">Catogary 2</button>
-                    <button className="btn btn-outline-dark me-2">Catogary 3</button>
+                    <button className="btn btn-outline-dark me-2" onClick={() => setFilter(data)}>All</button>
+                    <button className="btn btn-outline-dark me-2" onClick={() => filterProduct("catagory 1")}>Catagory 1  </button>
+                    <button className="btn btn-outline-dark me-2" onClick={() => filterProduct("catagory 2")}>Catagory 2</button>
+                    <button className="btn btn-outline-dark me-2" onClick={() => filterProduct("catagory 3")}>Catagory 3</button>
+                    <button className="btn btn-outline-dark me-2" onClick={() => filterProduct("catagory 4")}>Catagory 4</button>
                 </div>
                 {filter.map((product) => {
                     return (
                         <>
                             <div className="col-md-3 mb-4">
                                 <div class="card h-100 text-center p-4" key={product.id} >
-                                    <img src={product.image} class="card-img-top" alt={product.title} height = "250px"/>
-                                    <div class ="card-body">
-                                    <h5 class ="card-title mb-0">{product.title.substring(0,12)}...</h5>
-                                    <p class ="card-text lead fw-bold">
-                                        $ {product.price}
-                                    </p>
-                                    <a href="#" class ="btn btn-outline-dark">Buy Now</a>
+                                    <img src={product.image} class="card-img-top" alt={product.title} height="250px" />
+                                    <div class="card-body">
+                                        <h5 class="card-title mb-0">{product.title.substring(0, 12)}...</h5>
+                                        <p class="card-text lead fw-bold">
+                                            $ {product.price}
+                                        </p>
+                                        <a href="#" class="btn btn-outline-dark">Buy Now</a>
                                     </div>
                                 </div>
                             </div>
@@ -60,8 +67,8 @@ const Products = () => {
                     )
                 })}
             </>
-        )
-    }
+        );
+    };
     return (
         <div>
             <div className="container my-5 py-5">
@@ -80,7 +87,3 @@ const Products = () => {
 }
 
 export default Products;
-
-
-
-
